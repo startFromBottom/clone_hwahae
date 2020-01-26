69cd34ff654a373083b25c3c9158d4f4690f4b54
startFromBottom <uhh0701@gmail.com>
2020-01-26 20:37:59 +0900

    add elasticbeanstalk

--- myapp/config/settings/production.py
+++ myapp/config/settings/production.py
@@ -1,24 +1,13 @@
 import os
 from .base import *
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'HOST': os.environ['MYSQL_ROOT_HOST'],
-#         'NAME': os.environ['MYSQL_DATABASE'],
-#         'USER': os.environ['MYSQL_USER'],
-#         'PASSWORD': os.environ['MYSQL_ROOT_PASSWORD'],
-#     }
-# }
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "HOST": "127.0.0.1",
-        "NAME": "cosmetic_db",
-        "USER": "programmer",
-        "PASSWORD": "1234",
-        "PORT": "3306",
-        "TEST": {"NAME": "test_cosmetic_db",},
+        "HOST": os.environ["MYSQL_ROOT_HOST"],
+        "NAME": os.environ["MYSQL_DATABASE"],
+        "USER": os.environ["MYSQL_USER"],
+        "PASSWORD": os.environ["MYSQL_ROOT_PASSWORD"],
     }
 }
+
