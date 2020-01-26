ef583185270ef66631af61b5ccfcb5d1b84a2496
startFromBottom <uhh0701@gmail.com>
2020-01-26 22:46:22 +0900

    add

--- myapp/config/settings/production.py
+++ myapp/config/settings/production.py
@@ -1,27 +1,13 @@
 import os
 from .base import *
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "HOST": os.environ["MYSQL_ROOT_HOST"],
-#         "NAME": os.environ["MYSQL_DATABASE"],
-#         "USER": os.environ["MYSQL_USER"],
-#         "PASSWORD": os.environ["MYSQL_ROOT_PASSWORD"],
-#         "PORT": "3306",
-#     }
-# }
-
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "HOST": "127.0.0.1",
-        "NAME": "cosmetic_db",
-        "USER": "programmer",
-        "PASSWORD": "1234",
+        "HOST": os.environ["MYSQL_ROOT_HOST"],
+        "NAME": os.environ["MYSQL_DATABASE"],
+        "USER": os.environ["MYSQL_USER"],
+        "PASSWORD": os.environ["MYSQL_ROOT_PASSWORD"],
         "PORT": "3306",
-        "OPTIONS": {"charset": "utf8mb4"},
-        "TEST": {"NAME": "test_cosmetic_db",},
     }
 }
